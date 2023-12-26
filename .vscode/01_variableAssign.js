1b7b5e2208c269217dcfce6ed0b4bd68a8d567eb
Sakshigarudi <sakshigarudi2424@gmail.com>
2023-12-26 16:29:43 +0530

    Basics Of JS
    Basics Of JS

---
+++ .vscode/01_variableAssign.js
@@ -0,0 +1,27 @@
+console.log("undefined");
+var myFullname= "Sakshi Garudi";
+console.log("My Full Name    :", myFullname);
+
+var myFullname= "Sakshi Digambar Garudi";
+console.log( "My Full Name is :", myFullname);
+
+console.log("I Love Only JS");
+
+var firstName = "Sakshi";
+var lastName = "Garudi";
+var Age = "22";
+var Address = "Hinjewadi, Pune";
+Pincode = "411057";
+var Gender = "Female";
+var mariatalStatus = "Unmarried";
+
+console.log("First Name      :", firstName);
+console.log("Last Name       :", lastName);
+console.log("Age             :", Age);
+console.log("Address         :", Address);
+console.log("Pincode         :", Pincode);
+console.log("Gender          :", Gender);
+console.log("Mariatal Status :", mariatalStatus);
+
+
+
---
+++ .vscode/helloWord.js
@@ -0,0 +1,18 @@
+console.log("Hello JS, I am Sakshi");
+
+var num = "10";   //variable Declaration
+console.log(num);  //undefined
+
+var myName = "Sakshi"; 
+console.log(myName);
+
+myName = "Sakshi Garudi";   //variable initialization
+console.log("First Name", myName);
+
+console.log("Full name", myName);
+
+var age = "22";
+console.log("age:", age);
+
+myName = "Sakshi Garudi";  //variable update
+console.log("Full name: ", myName);
